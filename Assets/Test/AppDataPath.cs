77eaf37762ae24c9ec748983cf037c13c1e5070b
ALEX-WHISPER <15196608305@163.com>
2018-06-07 17:45:42 +0800

    1. Change LuaConst.luaDir to Application.dataPath + /LuaFramework/Lua, 2. DebugMode: AppConst.DebugMode = true, ExampleMode = true, UpdateMode = false, LuaBundleMode = false

---
+++ Assets/Test/AppDataPath.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppDataPath : MonoBehaviour {
+
+    private void Start() {
+        Debug.Log(string.Format("--- Application.dataPath: {0}---", Application.dataPath));
+    }
+}
